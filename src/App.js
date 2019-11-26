ef85540515e3127aa40611b8ffa29b596672085b
GillianMcDonald <52549335+GillianMcDonald@users.noreply.github.com>
2019-11-26 14:44:31 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import img from './img.jpg';
-
-
-
+import Blocks from './components/Blocks.js'
 
 class App extends React.Component {
-    state ={
+  constructor(props){
+    super(props);
+    this.state = {
       box1: true,
       box2: true,
       box3: true,
@@ -17,68 +17,65 @@ class App extends React.Component {
       box8: true,
       box9: true
       }
+    }
 
+    mouseClick = (box) => {
+      if(box === "box1")
+      {
+        this.setState({
+          box1: false
+        })
+      }
+      if(box === "box2")
+      {
+        this.setState({
+          box2: false
+        })
+      }
+      if(box === "box3")
+      {
+        this.setState({
+          box3: false
+        })
+      }
+      if(box === "box4")
+      {
+        this.setState({
+          box4: false
+        })
+      }
+      if(box === "box5")
+      {
+        this.setState({
+          box5: false
+        })
+      }
+      if(box === "box6")
+      {
+        this.setState({
+          box6: false
+        })
+      }
+      if(box === "box7")
+      {
+        this.setState({
+          box7: false
+        })
+      }
+      if(box === "box8")
+      {
+        this.setState({
+          box8: false
+        })
+      }
+      if(box === "box9")
+      {
+        this.setState({
+          box9: false
+        })
+      }
+    }  
 
-
-mouseClick = (box) => {
-  if(box === "box1")
-  {
-    this.setState({
-      box1: false
-    })
-  }
-  if(box === "box2")
-  {
-    this.setState({
-      box2: false
-    })
-  }
-  if(box === "box3")
-  {
-    this.setState({
-      box3: false
-    })
-  }
-  if(box === "box4")
-  {
-    this.setState({
-      box4: false
-    })
-  }
-  if(box === "box5")
-  {
-    this.setState({
-      box5: false
-    })
-  }
-  if(box === "box6")
-  {
-    this.setState({
-      box6: false
-    })
-  }
-  if(box === "box7")
-  {
-    this.setState({
-      box7: false
-    })
-  }
-  if(box === "box8")
-  {
-    this.setState({
-      box8: false
-    })
-  }
-  if(box === "box9")
-  {
-    this.setState({
-      box9: false
-    })
-  }
-  
-  
- 
-}
 
 
   render() {
@@ -86,17 +83,8 @@ mouseClick = (box) => {
     return (
       <div className="MumContainer">   
         <img src={img} alt="dingbat" />   
-        <div className="Container"> 
-          {this.state.box1 ? <button onClick={() => this.mouseClick("box1")} className="box box1">1</button> : <div className="Empty"></div>} 
-          {this.state.box2 ? <button onClick={() => this.mouseClick("box2")} className="box box2">2</button> : <div className="Empty"></div>} 
-          {this.state.box3 ? <button onClick={() => this.mouseClick("box3")} className="box box3">3</button> : <div className="Empty"></div>} 
-          {this.state.box4 ? <button onClick={() => this.mouseClick("box4")} className="box box4">4</button> : <div className="Empty"></div>} 
-          {this.state.box5 ? <button onClick={() => this.mouseClick("box5")} className="box box5">5</button> : <div className="Empty"></div>} 
-          {this.state.box6 ? <button onClick={() => this.mouseClick("box6")} className="box box6">6</button> : <div className="Empty"></div>} 
-          {this.state.box7 ? <button onClick={() => this.mouseClick("box7")} className="box box7">7</button> : <div className="Empty"></div>} 
-          {this.state.box8 ? <button onClick={() => this.mouseClick("box8")} className="box box8">8</button> : <div className="Empty"></div>} 
-          {this.state.box9 ? <button onClick={() => this.mouseClick("box9")} className="box box9">9</button> : <div className="Empty"></div>} 
-        </div>
+        <Blocks box1={this.state.box1} box2={this.state.box2} box3={this.state.box3} box4={this.state.box4} box5={this.state.box5} box6={this.state.box6} 
+        box7={this.state.box7} box8={this.state.box8} box9={this.state.box9} function={this.mouseClick}/>
       </div>
     )
   }
@@ -104,3 +92,5 @@ mouseClick = (box) => {
 
 
 export default App
+
+
--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -1,20 +1,22 @@
-// import React from 'react'
-// import '../Styles/Blocks.css'
+import React from 'react'
 
-// export const Blocks = (props) => {
-//     return (
-//         <div className="container">
-//             <button key="1" onClick={() => props.function(1)} className="box box1">1</button>
-//             <button key="2" onClick={() => props.function(2)} className="box box2">2</button>
-//             <button key="3" onClick={() => props.function(3)} className="box box3">3</button>
-//             <button key="4" onClick={() => props.function(4)} className="box box4">4</button>
-//             <button key="5" onClick={() => props.function(5)} className="box box5">5</button>
-//             <button key="6" onClick={() => props.function(6)} className="box box6">6</button>
-//             <button key="7" onClick={() => props.function(7)} className="box box7">7</button>
-//             <button key="8" onClick={() => props.function(8)} className="box box8">8</button>
-//             <button key="9" onClick={() => props.function(9)} className="box box9">9</button>
-//         </div>
-//     )
-// }
+export const Blocks = (props) => {
+    return (
 
-// export default Blocks
+
+        <div className="Container"> 
+        {props.box1 ? <button onClick={() => props.function("box1")} className="box box1">1</button> : <div className="Empty"></div>} 
+        {props.box2 ? <button onClick={() => props.function("box2")} className="box box2">2</button> : <div className="Empty"></div>} 
+        {props.box3 ? <button onClick={() => props.function("box3")} className="box box3">3</button> : <div className="Empty"></div>} 
+        {props.box4 ? <button onClick={() => props.function("box4")} className="box box4">4</button> : <div className="Empty"></div>} 
+        {props.box5 ? <button onClick={() => props.function("box5")} className="box box5">5</button> : <div className="Empty"></div>} 
+        {props.box6 ? <button onClick={() => props.function("box6")} className="box box6">6</button> : <div className="Empty"></div>} 
+        {props.box7 ? <button onClick={() => props.function("box7")} className="box box7">7</button> : <div className="Empty"></div>} 
+        {props.box8 ? <button onClick={() => props.function("box8")} className="box box8">8</button> : <div className="Empty"></div>} 
+        {props.box9 ? <button onClick={() => props.function("box9")} className="box box9">9</button> : <div className="Empty"></div>} 
+      </div>
+    
+    )
+}
+
+export default Blocks
